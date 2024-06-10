d3e9c57d84e130748ce616ab32b11c3e7cf334f6
alex4401 <rylatgl@gmail.com>
2024-06-11 01:36:31 +0200

    feat: affect User::isBot

--- src/Hooks/PermissionsHooks.php
+++ src/Hooks/PermissionsHooks.php
@@ -5,7 +5,8 @@ use MediaWiki\Extension\GlobalMessages\GlobalPermissionsRegistry;
 use User;
 
 final class PermissionsHooks implements
-    \MediaWiki\User\Hook\UserEffectiveGroupsHook
+    \MediaWiki\User\Hook\UserEffectiveGroupsHook,
+	\MediaWiki\User\Hook\UserIsBotHook
 {
 	private GlobalPermissionsRegistry $registry;
 
@@ -24,6 +25,22 @@ final class PermissionsHooks implements
 		if ( $user->isRegistered() ) {
 			$groups += $this->registry->getGroupsForUser( $user );
 			$groups = array_unique( $groups );
+		}
+	}
+
+	/**
+	 * @param User $user
+	 * @param bool &$isBot Whether this is user a bot or not (boolean)
+	 * @return bool|void True or no return value to continue or false to abort
+	 */
+	public function onUserIsBot( $user, &$isBot ) {
+		if ( $user->isRegistered() ) {
+			foreach ( $this->registry->getGroupsForUser( $user ) as $group ) {
+				if ( str_contains( $group, '-bot' ) ) {
+					$isBot = true;
+					break;
+				}
+			}
 		}
 	}
 }
